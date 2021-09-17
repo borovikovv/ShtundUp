eb8f0753921f76074d305e27df19734bca8f5696
Borovikov <borovikovvaleksey@gmail.com>
2021-09-17 18:47:20 +0300

    add config file

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,21 +2,29 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './controllers/users/users.controller';
 import { AppService } from './app.service';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+const configure = new ConfigService();
 
 @Module({
   controllers: [UsersController],
   providers: [AppService],
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
+      isGlobal: true
+    }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'olexii',
-      password: 'SudoPass7573User',
-      database: 'shtundup',
+      host: configure.get("HOST"),
+      port: Number(configure.get("POSTGRES_PORT")),
+      username: configure.get("POSTGRES_NAME"),
+      password: configure.get("POSTGRES_PASSWORD"),
+      database: configure.get("POSTGRES_DB"),
       models: [],
       autoLoadModels: true
     }),
   ]
 })
+
 export class AppModule {}
